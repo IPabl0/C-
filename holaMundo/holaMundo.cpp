010c2aa9cba335a641371f288c2aba417a3e0077
Pablo_Ortega <p.ortega09@gmail.com>
2017-11-15 20:10:16 -0300

    Hola Mundo

---
+++ holaMundo/holaMundo.cpp
@@ -0,0 +1,19 @@
+//Hola Mundo
+
+//Librerias
+//es un conjunto de implementaciones funcionales
+#include <iostream>
+
+//namespace
+/*
+los Namespaces se componen de un conjunto de objetos relacionados 
+como clases, delegados, estructuras, interfaces
+*/
+using namespace std;
+
+int main (){
+	
+	cout <<"Hola Mundo! :)" <<endl; //endl nos da un salto de linea
+	
+	return 0;
+}
