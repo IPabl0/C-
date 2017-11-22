ad604adb4880e2513b92d00d663d027d91ec5076
Pablo_Ortega <p.ortega09@gmail.com>
2017-11-21 22:40:37 -0300

    Ejercicio leer un caracter y determinar si es vocal minuscula o no

---
+++ Ejercicios/Ejercicio18/Ejercicio18.cpp
@@ -0,0 +1,24 @@
+/*
+Leer 1 entero determinar
+si es par o impar
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+char letra;
+
+cout<<"Digite un caracter: ";cin>>letra;
+
+switch(letra){
+	case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u': cout<<"Es una vocal minuscula";break;
+	default : cout<<"No es una vocal minuscula"; break;	
+}
+    return 0;
+}
